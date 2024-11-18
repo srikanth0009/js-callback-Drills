9421afbb4acf94f3d80e7b86ad1b3c6102ed4b02
srikanth <srikanthchowdary8499@gmail.com>
2024-11-18 11:17:45 +0530

    modification done calling function

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -3,5 +3,11 @@ import { createDirectory,createFiles, deleteFiles } from "../problem1.js";
 
 
 
-createDirectory(createFiles, deleteFiles);
+createDirectory(()=>{
+    createFiles(()=>{
+        deleteFiles(()=>{
+            console.log("creation and deletion of files completed");
+        });
+    })
+})
 
